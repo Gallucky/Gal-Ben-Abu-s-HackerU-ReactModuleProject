c3a2c4a498ae99ca4ca4b59604aa355f18828595
Gallucky <galbenabu1111@email.com>
2025-04-05 16:38:08 +0300

    quickfix

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,5 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Gal-Ben-Abu-s-HackerU-ReactModuleProject/",
   plugins: [react()],
 });
